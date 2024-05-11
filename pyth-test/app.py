4f4c16a55fbd0ec0edeac82e7928b17ef570cdcb
Harshi <hveerava@andrew.cmu.edu>
2024-05-11 17:40:36 +0800

    ytc static folder

--- pyth-test/app.py
+++ pyth-test/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, request
-import numpy as np
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -10,10 +8,6 @@ def index():
     ydata=[]
     xerr =[]
     yerr =[]
-    '''names = []
-    emails = []
-    phones = []
-    addresses = []'''
 
     if request.method == 'POST':
         form_data = request.form
@@ -28,31 +22,12 @@ def index():
                 xerr.append(value)
             elif field == 'yerr':
                 yerr.append(value)
-        '''print(xdata)
-        print(ydata)
-        print(xerr)
-        print(yerr)'''
-        # ylsf stuff slow integration
+
         for i in range(len(xdata)):
             xdata[i] = float(xdata[i])
             ydata[i] = float(ydata[i])
             xerr[i] = float(xerr[i])
             yerr[i] = float(yerr[i])
-
-        '''plt.errorbar(xdata, ydata, yerr=yerr, fmt='o', label='Data')
-
-        # Fit a best fit line (linear regression) to the data
-        coefficients = np.polyfit(xdata, ydata, 1)
-        best_fit_line = np.poly1d(coefficients)
-        x_values = np.linspace(min(xdata), max(xdata), 100)
-        plt.plot(x_values, best_fit_line(x_values), label='Best Fit Line')
-
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        plt.title('Best Fit Line with Error Bars')
-        plt.legend()
-        plt.grid(True)
-        plt.savefig('image1.jpeg')'''
 
         return render_template('result.html', xdata=xdata, ydata=ydata, xerr=xerr, yerr=yerr)
     
